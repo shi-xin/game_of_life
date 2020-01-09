ea5bbfc87ce57c9f41e061ea0f8f4477ed3477f3
Shi Xin <stevexinshi@gmail.com>
2020-01-09 16:20:35 +0800

    2-d game of life
    sort of works now

--- gol.py
+++ gol.py
@@ -7,8 +7,17 @@
 # This probably isn't how individual living thing live, but it certainly emits
 # that illusion. A fun project indeed.
 
+# Rules
+# 1 - any live cell with fewer than two live neighbors dies, as if caused by under-population
+# 2 - any live cell with two or three live neighbors lives on to the next generation
+# 3 - any live cell with more than three live neighbors dies, as if by over-population
+# 4 - any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
 
-# Structure
+# Update Generation
+# these four rules are applied to all cells simultaneously
+# all cells are updated at the same time from the same initial state
+
+# Code Structure
 # - initial state: either randomly generated or let user specify
 # - a evolve function: input a matrix that represent the living thing, and
 #   calculate how each element should behave according to the set of rules
@@ -17,13 +26,104 @@
 #   iteration.
 
 
+# Further, a 3D version of this..
+
+
 import numpy
-import pil
-import random
+import matplotlib.pyplot as plt
 
 class GameOfLife:
 
     def __init__(self):
         """ initial state """
-        
-        
+        self.fig = plt.figure()
+        self.ax = self.fig.gca()
+        self.fig.show()
+        # randomly generate an initial state
+        self.population = numpy.random.randint(2, size = (100, 100))
+        # [TODO] let user define the initial state
+    
+    def evolve(self):
+        """ use evolution rules to evolute the input map """
+        population_buffer = self.population
+        i, j = population_buffer.shape
+        cells = numpy.array(numpy.meshgrid(range(i), range(j))).T.reshape(-1, 2)
+        for coord in cells:
+            # coordinate of a cell: n[0], n[1]
+            if coord[0] == 0:
+                # top row
+                if coord[1] == 0:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] + 1, coord[1]:(coord[1]+1)])
+                elif coord[1] == j - 1:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    sum(population_buffer[coord[0] + 1, (coord[1]-1):coord[1]])
+                else:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] + 1, (coord[1] - 1):(coord[1]+1)])
+            elif coord[0] == i - 1:
+                # bottom row
+                if coord[1] == 0:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] - 1, coord[1]:(coord[1]+1)])
+                elif coord[1] == j - 1:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    sum(population_buffer[coord[0] - 1, (coord[1]-1):coord[1]])
+                else:
+                    neighbor_sum = \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] - 1, (coord[1] - 1):(coord[1]+1)])
+            else:
+                if coord[1] == 0:
+                    neighbor_sum = \
+                    sum(population_buffer[coord[0] - 1, (coord[1]):(coord[1]+1)]) + \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] + 1, (coord[1]):(coord[1]+1)])
+                elif coord[1] == j - 1:
+                    neighbor_sum = \
+                    sum(population_buffer[coord[0] - 1, (coord[1] - 1):(coord[1])]) + \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    sum(population_buffer[coord[0] + 1, (coord[1] - 1):(coord[1])])
+                else:
+                    neighbor_sum = \
+                    sum(population_buffer[coord[0] - 1, (coord[1] - 1):(coord[1]+1)]) + \
+                    population_buffer[coord[0], coord[1] - 1] + \
+                    population_buffer[coord[0], coord[1] + 1] + \
+                    sum(population_buffer[coord[0] + 1, (coord[1] - 1):(coord[1]+1)])
+
+            # apply rules now
+            if self.population[coord[0], coord[1]] == 1:
+                # what happens to live cell
+                if neighbor_sum < 2 or neighbor_sum > 3:
+                    self.population[coord[0], coord[1]] = 0
+                else:
+                    self.population[coord[0], coord[1]] = 1
+            else:
+                # what happens to dead cell
+                if neighbor_sum == 3:
+                    self.population[coord[0], coord[1]] = 1
+                else:
+                    self.population[coord[0], coord[1]] = 0
+
+
+    def play(self):
+        # display image
+        for i in range(100000):
+            plt.cla() # clear axes
+            self.ax.axis('off') # hide x, y axes
+            self.ax.imshow(self.population, cmap = plt.cm.gray)
+            self.fig.canvas.draw()
+            self.evolve()
+            plt.pause(0.1)
+
+
+if (__name__ == '__main__'):
+    game = GameOfLife()
+    game.play()
---
+++ test_image_libs.py
@@ -0,0 +1,20 @@
+# testing image output libraries
+# matplotlib
+# this does the basic job: produce and refresh the plot with new data
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+# generate figure
+fig = plt.figure()
+ax = fig.gca()
+fig.show()
+
+# refresh plot
+for i in range(100):
+    plt.cla() # clear axes
+    ax.axis('off') # hide x, y axes
+    image = np.random.randint(2, size = (100, 100)) # data
+    ax.imshow(image, cmap = plt.cm.gray)
+    fig.canvas.draw()
+    plt.pause(0.1)
